81ae85719d9651991b485c06dc51152a5aae6353
Callum Macdonald <git@callum-macdonald.com>
2017-11-16 13:25:01 +0100

    Simple example reproduction.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 // @flow
 import React from "react";
 
+// This does not generate an error
+// import foo from "../services/foo";
+// This successfully produces an error
+import foo from "../services/foo/foo.service";
+
 const App = (props: {}): mixed => {
+  console.log(foo("alpha", "bravo"));
+
   return <div>Example</div>;
 };
 
